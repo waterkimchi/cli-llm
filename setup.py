b32fed4acb03c70cc2e456f29189aeb5a0844888
waterkimchi <hyunsul2@uci.edu>
2025-02-22 17:25:59 -0800

    fix: changed package logic

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(
-    name="clillm",
-    version="0.1.0",
-    packages=["clillm"],
-    install_requires=[
-        "typer",
-        "colorama",
-        "shellingham",
-        "pytest",
-    ],
-    entry_points={"console_scripts": ["clillm = clillm.__main__:main"]},
-)
+setup()
